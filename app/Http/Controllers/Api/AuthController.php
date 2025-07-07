f6fe2089c38acdc18cf1d27ae10b04f4122064f2
urboy-code <42699288+urboy-code@users.noreply.github.com>
2025-07-07 23:06:32 +0700

    feat(auth): Initialize API project and set up Sanctum authentication

---
+++ app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rules;
+
+class AuthController extends Controller
+{
+    //
+    public function register(Request $request){
+        $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed', Rules\Password::default()],
+            'role' => ['required', 'string', 'in:client,talent'],
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'role' => $request->role,
+        ]);
+
+        // Buat token untuk user baru
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+            'user' => $user
+        ], 201);
+    }
+}
